389866b8c3b2e3d12dfe4a18542097d66a45d31e
Gani25 <memonabdulgani3d@gmail.com>
2025-11-05 19:47:05 +0530

    Input Examples

---
+++ input1.py
@@ -0,0 +1,11 @@
+# Take inpt from user
+
+name = input("Enter name: ")
+
+age = input("Enter age: ")
+
+print("--------------------------------------------")
+print("User Info")
+print("Name =",name)
+print("Age =",age)
+print("--------------------------------------------")
---
+++ input2.py
@@ -0,0 +1,8 @@
+# Accept 2 numbers and display their addition
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+sum = num1+num2 
+
+print("Addition of",num1,",",num2,"=",sum)
