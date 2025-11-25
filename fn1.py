a696171f9450507eff1a2df38cfb0f54f4c9f71b
Gani25 <memonabdulgani3d@gmail.com>
2025-11-25 20:00:16 +0530

    functions

---
+++ fn1.py
@@ -0,0 +1,17 @@
+# without parameter without return statements
+
+# Function definition
+def addition():
+    n1 = int(input("Enter number 1: "))
+    n2 = int(input("Enter number 2: "))
+
+    sum = n1 + n2
+    print(f"{n1} + {n2} = {sum}")
+
+
+print("Welcome")
+
+# Call
+addition()
+addition()
+addition()
---
+++ fn_without.py
@@ -0,0 +1,27 @@
+# WAP to accept 2 number and print sum -> do 4 times
+
+
+print("Welcome")
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+sum = n1 + n2
+print(f"{n1} + {n2} = {sum}")
+
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+sum = n1 + n2
+print(f"{n1} + {n2} = {sum}")
+
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+sum = n1 + n2
+print(f"{n1} + {n2} = {sum}")
+
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+sum = n1 + n2
+print(f"{n1} + {n2} = {sum}")
