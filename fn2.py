13da83e0980d89b13492d1ad501bc11537f05b0d
Gani25 <memonabdulgani3d@gmail.com>
2025-11-26 19:17:40 +0530

    functions

---
+++ fn2.py
@@ -0,0 +1,14 @@
+# without return statement with parameters
+
+def subtraction(n1, n2):
+
+    result = n1 - n2 
+
+    print(f"{n1} - {n2} = {result}")
+
+# subtraction(10,6)
+
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+subtraction(n1,n2)
---
+++ fn3.py
@@ -0,0 +1,9 @@
+def area_of_rectangle(length, breadth):
+    area = length * breadth
+
+    print(f"Area of Rectangle with \nlength: {length}, \nbreadth: {breadth} \nis {area} ")
+
+l = int(input("Enter length of a rectangle: "))
+b = int(input("Enter length of a breadth: "))
+
+area_of_rectangle(breadth = b,length = l)
